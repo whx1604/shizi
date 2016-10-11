aaca06a46a45a3ce5c365c3f624afd59c36dbf1a
李明文 <limw@csdn.net>
2016-10-11 16:02:36 +0800

    加入devise做用户注册

---
+++ db/migrate/20161011054742_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone_num, :string
+    add_column :users, :wechat, :string
+  end
+end
